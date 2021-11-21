0d242484c5debd2662519084abbff627ec5e48d6
klessitonrds <klessitom@gmail.com>
2021-11-21 06:46:23 -0300

    setup: strapi api

---
+++ packages/api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '097cada3f05d65ce2bec979051b6ff22'),
+    },
+  },
+});
---
+++ packages/api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '27c46256-83fd-4742-a2b5-8ff05dc94ec4'
+};
